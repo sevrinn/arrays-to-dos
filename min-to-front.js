23f892be7f604db5417bbc83dad6de22f6e3bbe2
Sevrinn <sevrinnsevrinn@gmail.com>
2021-09-03 11:17:42 -0700

    set up Min to Front assignment

---
+++ min-to-front.js
@@ -0,0 +1,8 @@
+/**
+ * **************** Min to Front ***************************
+ * Given an array of comparable values, move the lowest element to arrays front,
+ * shifting backward any element previeous ahead of it.
+ * Do not otherwise change the arrays order.
+ * Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it.
+ * Do this without using built-in functions.
+ */
